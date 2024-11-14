226aa44c6b9a6c61ba5a47b48060c1d750a07319
Henry-Ruiz <henryruiz686@gmail.com>
2024-11-14 02:06:19 -0400

    proyecto acabado

--- index.js
+++ index.js
@@ -8,8 +8,23 @@ app.set('port', process.env.PORT || 3000);
 //static files
 app.use(express.static(path.join(__dirname, 'public')));
 
-
 //start the server
-app.listen(app.get('port'), () =>{
+const server = app.listen(app.get('port'), () =>{
     console.log('server on port', app.get('port'));
 });
+
+const SocketIO = require('socket.io');
+const io = SocketIO(server);
+
+//webcokets
+io.on('connection', (socket) =>{
+console.log('new connection', socket.id);
+
+socket.on('chat:message', (data) =>{
+io.sockets.emit('chat:message', data);
+})
+socket.on('chat:typing', (data) =>{
+socket.broadcast.emit('chat:typing', data);
+})
+});
+
